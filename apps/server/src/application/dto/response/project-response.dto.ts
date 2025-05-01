ea910dda5d2dd8ce51efb9878180c680cda6d91c
Jyzdcs <ky.claudant@gmail.com>
2025-05-01 16:54:03 +0200

    feat: suppression des DTO et mappers liés aux réponses de projet, ajout de tests et de builders pour améliorer la structure des tests

--- apps/server/src/application/dto/response/project-response.dto.ts
+++
@@ -1,9 +0,0 @@
-export class ProjectResponseDto {
-  id: string | null;
-  title: string;
-  description: string;
-  status: string | null;
-  link: string | null;
-  userId: string;
-  techStacks: { id: string; name: string; iconUrl: string }[];
-}
